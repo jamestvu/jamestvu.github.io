ec9dfe7a0e9f729b0154de21bff9a2c44fe28186
James Vu <jamesvu@eworldes.com>
2015-09-07 20:00:39 -1000

    Added resource files, modules, a background, navbar, and routing.

---
+++ app.js
@@ -0,0 +1,8 @@
+﻿angular
+    .module('jamesVuWebsite',
+    ['ui.router', 'jamesVuWebsite.Routes', 'AboutModule', 'ContactModule', 'NavigationModule']);
+
+angular
+    .module('jamesVuWebsite')
+    .constant('applicationName', 'JamesVuWebsite')
+    .constant('versionNumber', '0.1');
---
+++ routes.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    function Routes($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/about');
+
+        $stateProvider
+            .state('about', {
+                url: '/about',
+                templateUrl: 'modules/about/about.html'
+            })
+
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'modules/contact/contact.html'
+            });
+    }
+    angular
+        .module('jamesVuWebsite.Routes', [])
+        .config(['$stateProvider', '$urlRouterProvider', Routes]);
+})();
