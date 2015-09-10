5bfe2f28b50fa77ede7804d2ce211b681bcf3992
James Vu <jamesvu@eworldes.com>
2015-09-09 22:08:19 -1000

    Properly renamed all js files.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 ﻿angular
     .module('JamesVuWebsite',
-    ['ui.router', 'JamesVuWebsite.Routes', 'AboutModule', 'ContactModule', 'NavigationModule']);
+    ['ui.router', 'JamesVuWebsite.Routes', 'AboutModule', 'ContactModule']);
 
 angular
     .module('JamesVuWebsite')
