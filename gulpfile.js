ab221c94a7ccd73e955613584ee7ea3c672c432c
James Vu <jamesvu@eworldes.com>
2015-09-10 21:07:09 -1000

    Added minify js to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,19 @@
 ﻿var gulp = require('gulp');
 
-var minifyHTML = require('gulp-minify-html');
+var minify = require('gulp-minify');
 
-gulp.task('minify-html', function () {
-    var opts = {
-        conditionals: true,
-        spare: true
-    };
-
-    return gulp.src('./static/html/*.html')
-      .pipe(minifyHTML(opts))
-      .pipe(gulp.dest('./dist/'));
+gulp.task('compress', function () {
+    gulp.src('lib/*.js')
+      .pipe(minify({
+          exclude: ['tasks'],
+          ignoreFiles: ['.combo.js', '-min.js']
+      }))
+      .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', function () {
-    
-    return console.info(gulp.src('*.html').pipe(minifyHTML()).pipe(gulp.dest('dest')));
-});
+//gulp.task('default', function () {
+//    return console.info(gulp.src('*.html').pipe(minifyHTML()).pipe(gulp.dest('dest')));
+//});
 
 //gulp.task('potato', function () {
 //    console.info('running potato');
