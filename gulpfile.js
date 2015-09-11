4c598f54079854200c2334c405eda056e24fc638
James Vu <jamesvu@eworldes.com>
2015-09-10 21:55:52 -1000

    Got gulp working for minifying js files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
 ﻿var gulp = require('gulp');
-
+var uglify = require('gulp-uglify');
 var minify = require('gulp-minify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 
 gulp.task('compress', function () {
-    gulp.src('lib/*.js')
+    gulp.src('*/*.js')
+      .pipe(concat('jamesvu-website.js'))
       .pipe(minify({
           exclude: ['tasks'],
-          ignoreFiles: ['.combo.js', '-min.js']
+          ignoreFiles: ['.combo.js', '-min.js', 'gulpfile.js']
       }))
       .pipe(gulp.dest('dist'))
 });
+
+gulp.task('scripts', function () {
+    return gulp.src(['./*.js', '!gulpfile.js', 'modules/**/*.js',])
+      .pipe(concat('jamesvu-website.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest(''));
+});
+// Default Task
+gulp.task('default', ['scripts']);
 
 //gulp.task('default', function () {
 //    return console.info(gulp.src('*.html').pipe(minifyHTML()).pipe(gulp.dest('dest')));
