81ee204622587cde2fd4b2afce32784c92531f59
James Vu <jamesvu@eworldes.com>
2015-09-16 21:55:33 -1000

    Updated about page and resume page.

--- modules/resume/resume-controller.js
+++ modules/resume/resume-controller.js
@@ -1,13 +1,21 @@
-﻿//(function () {
-//    function ResumeCtrl($scope) {
-//        $scope.one
-//    }
-//    angular
-//        .module('ResumeModule')
-//        .controller('ResumeCtrl', ['$scope', SplashCtrl]);
-//})();
+﻿(function () {
+    function ResumeCtrl($scope, $http) {
+        var vm = this;
+        this.hello = 'quack';
+        $scope.hello = 'bark';
+        $http.get('modules/resume/resume.json')
+        .then(function (response) {
+            console.info(response)
+            vm.resume = response.data;
+        });
+
+    }
+    angular
+        .module('ResumeModule')
+        .controller('ResumeCtrl', ['$scope', '$http', ResumeCtrl]);
+})();
 
 
-angular.module('ResumeModule').controller('ResumeCtrl', function ($scope) {
-    $scope.status = false;
-});
+//angular.module('ResumeModule').controller('ResumeCtrl', function ($scope) {
+//    $scope.status = false;
+//});
